10cf33b5f046fb2efd6d76c504c563014ebdd86d
Andrii Zadorozhnii <doxtop@synrc.com>
2017-02-05 03:29:02 +0200

    finaly working application inject into PlaySpec

--- src/test/scala/webSpec.scala
+++ src/test/scala/webSpec.scala
@@ -6,32 +6,32 @@ import org.scalatest._
 import org.scalatest.concurrent._
 import org.scalatestplus.play._
 
+import play.api.{Play, Application}
+import play.api.routing.sird._
+import play.api.routing._
 import play.api.mvc._
+import play.api.mvc.Results._
 import play.api.test._
-import play.api.test.Helpers._
-
-import play.api.inject._
-import play.api.inject.guice._
-
-import controllers._
 
 import play.api.inject.guice.GuiceApplicationBuilder
 
-import scala.language.implicitConversions
+import controllers._
 
-class ControllerSpec extends PlaySpec with Results with ScalaFutures {
+class ControllerSpec extends PlaySpec with Results with ScalaFutures with OneAppPerSuite {
 
-  // implicit override lazy val app = new GuiceApplicationBuilder()
-  //   .bindings(new PlayTestModule)
-  //   .build
+  implicit override lazy val app = new GuiceApplicationBuilder()
+    .bindings(new PlayTestModule)
+    .configure(Map("s" -> "s"))
+    .router(Router.from{
+      case GET(p"/test") => 
+        Action(Results.Ok("fucking test"))
+    })
+    .build
 
-  // "Example Page#get" should {
+  "The OneAppPerSuite trait" must {
+    "provide a FakeApplication" in {
+      app.configuration.getString("s") mustBe Some("s")
+    }
+  }
 
-  //   "should be valid" in {
-  //     val controller = app.injector.instanceOf[HomeController]  
-  //     val result: Future[Result] = controller.get().apply(FakeRequest())
-  //     val bodyText: String = contentAsString(result)
-  //     bodyText mustBe "ok"
-  //   }
-  // }
 }
