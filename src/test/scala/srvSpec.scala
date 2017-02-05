3f0d2eb8703d01d66e1f42391e5db33ffadccb34
Andrii Zadorozhnii <doxtop@synrc.com>
2017-02-05 02:19:35 +0200

    update scalatestplus library to 2.0.0-M1 for scalatest 3.0.1 support

--- src/test/scala/srvSpec.scala
+++ src/test/scala/srvSpec.scala
@@ -2,8 +2,14 @@ package adv
 
 import org.scalatest._
 
+/**
+ * Adverts service layer specification.
+ *
+ * Sorting adverts list functionality.
+ */
 class SrvSpec extends FunSpec{
-  describe ("Adverts service") {
-    it ("should 1") (pending)
+  describe ("service layer") {
+    it ("proxy every operatoin to dba") (pending)
+    it ("various list sort check") (pending)
   }  
 }
